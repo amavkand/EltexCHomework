1246b0ba7a7a26c3bcfb6511da7b0fbd2d7789c9
admin <amavkand@Linux.amavkand.virtualbox.org>
2024-07-29 11:53:19 +0700

    Edits homework 3

--- Pointers/exercise4.c
+++ Pointers/exercise4.c
@@ -4,7 +4,8 @@
 int main() {
   char string[100];
   char substring[50];
-  char *ptr = NULL; 
+  char *ptr = NULL;
+  int i, j, k;
 
   printf("Введите строку: ");
   fgets(string, sizeof(string), stdin);
@@ -14,11 +15,25 @@ int main() {
 
   substring[strcspn(substring, "\n")] = '\0';
 
-  ptr = strstr(string, substring);
+  for (i = 0; string[i] != '\0'; i++) {
+    if (string[i] == substring[0]) {
+      for (j = 1, k = i + 1; substring[j] != '\0'; j++, k++) {
+        if (string[k] != substring[j]) {
+          break;
+        }
+      }
+      if (substring[j] == '\0') {
+        ptr = &string[i];
+        break;
+      }
+    }
+  }
 
   if (ptr != NULL) {
     printf("Подстрока найдена по адресу: %p\n", ptr);
   } else {
     printf("Подстрока не найдена.\n");
   }
+
+  return 0;
 }
